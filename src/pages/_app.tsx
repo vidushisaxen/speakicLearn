a1b9377642c24cb94d349712ab031b492493f295
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-08-15 10:10:30 +0530

    main

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { type AppType } from "next/dist/shared/lib/utils";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+import "../styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <title>Speakic Learn</title>
+        <meta
+          name="description"
+          content="Speakic web app clone written with React"
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="theme-color" content="#0A0" />
+        <link rel="manifest" href="/app.webmanifest" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
