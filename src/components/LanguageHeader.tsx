f81ae1472bcc101aa1762e4917d8cc4d338f2d71
vidushisaxen <vidushisaxenabdn786@gmail.com>
2024-08-15 22:37:41 +0530

    main

--- src/components/LanguageHeader.tsx
+++ src/components/LanguageHeader.tsx
@@ -4,7 +4,7 @@ import { LanguageDropDown } from "./LanguageDropDown";
 export const LanguageHeader = () => {
   return (
     <header className="fixed left-0 right-0 top-0 mx-auto flex min-h-[70px] max-w-5xl items-center justify-between bg-[#88038F] px-10 font-bold text-white">
-      <Link className="text-4xl" href="/">
+      <Link className="text-4xl" href="https://vocalize-eight.vercel.app/">
         Speakic
       </Link>
       <LanguageDropDown />
